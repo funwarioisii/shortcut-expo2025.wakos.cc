9072546d15055fce75b06d4f5c96be362be98fc3
funwarioisii <kazuyukihashimoto2006@gmail.com>
2025-05-07 18:41:14 +0900

    initial commit

---
+++ doc/sw.js
@@ -0,0 +1,64 @@
+const CACHE_NAME = 'expo2025-shortcut-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/favicon.ico',
+  '/icons/icon-192x192.png',
+  '/icons/icon-512x512.png'
+];
+
+// インストール時にリソースをキャッシュ
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// キャッシュからリソースを取得、なければネットワークから取得
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request).then(
+          response => {
+            // 有効なレスポンスかチェック
+            if (!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+
+            // レスポンスをクローンしてキャッシュに追加
+            const responseToCache = response.clone();
+            caches.open(CACHE_NAME).then(cache => {
+              cache.put(event.request, responseToCache);
+            });
+
+            return response;
+          }
+        );
+      })
+  );
+});
+
+// 古いキャッシュを削除
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+}); 
